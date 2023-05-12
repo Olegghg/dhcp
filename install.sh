3dea41f83654c231b1fd350d6345362d8fcfa6d9
Olegghg <99468773+Olegghg@users.noreply.github.com>
2023-05-12 13:11:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,3 +15,5 @@ cp -rf http/site.conf /etc/apache2/sites-enabled/000-default.conf
 systemctl restart isc-dhcp-server
 systemctl restart tftpd-hpa
 systemctl restart apache2
+
+apt install -y ansible
