17c50e67b1a7ce999b2c4bd7b2bf519f49fa0bbd
Olegghg <olerp13361@gmail.com>
2023-05-13 04:07:49 -0400

    Update

---
+++ add.py
@@ -0,0 +1,57 @@
+import re
+import os
+while True:
+    # Запрашиваем данные
+    hostname = input("Введите имя хоста: ")
+    mac_address = input("Введите MAC-адрес (в формате xx:xx:xx:xx:xx:xx): ")
+    ip_address = input("Введите IP-адрес: ")
+
+    #проверка MAC
+    mac_regex_format = re.compile(r"^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$")
+    if not mac_regex_format.match(mac_address):
+        print("Ошибка: MAC-адрес не соответствует формату xx:xx:xx:xx:xx:xx")
+        exit(1)
+
+    #Проверка IP
+    ip_regex_format = re.compile(r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+    if not ip_regex_format.match(ip_address):
+        print("Ошибка: IP-адрес не соответствует формату x.x.x.x, где x от 0 до 255")
+        exit(1)
+
+    # Проверяем, существует ли файл dhcpd.conf
+    if not os.path.isfile("/etc/dhcp/dhcpd.conf"):
+        print("Файл dhcpd.conf не найден")
+        exit(1)
+
+    # Читаем содержимое файла
+    with open("/etc/dhcp/dhcpd.conf", "r") as file:
+        dhcpd_content = file.read()
+
+    # Создаем регулярные выражения для поиска
+    hostname_regex = re.compile(r"host\s+" + re.escape(hostname) + r"\s+\{")
+    mac_regex = re.compile(r"hardware ethernet\s+" + re.escape(mac_address) + r";")
+    ip_regex = re.compile(r"fixed-address\s+" + re.escape(ip_address) + r";")
+
+    # Проверяем, есть ли совпадения
+    if hostname_regex.search(dhcpd_content):
+        print(f"Ошибка: Имя хоста {hostname} уже настроено")
+    elif mac_regex.search(dhcpd_content):
+        print(f"Ошибка: MAC-адрес {mac_address} уже настроен")
+    elif ip_regex.search(dhcpd_content):
+        print(f"Ошибка: IP-адрес {ip_address} уже настроен")
+    else:
+        # Если совпадений нет, добавляем новую запись
+        with open("/etc/dhcp/dhcpd.conf", "a") as file:
+            file.write(f"\nhost {hostname} {{\n  hardware ethernet {mac_address};\n  fixed-address {ip_address};\n}}\n")
+
+        print("Запись добавлена")
+
+    # Формируем строку ansible
+    ansible_line = f"{hostname} ansible_host={ip_address} ansible_user=ansibleuser ansible_ssh_private_key_file=~/.ssh/id_rsa"
+
+    # Записываем строку в файл ansible/hosts.ini
+    with open("ansible/hosts.ini", "a") as file:
+        file.write(ansible_line + "\n")
+    nextstr = input("Введите y для создания еще 1 записи: ")
+    if nextstr!="y":
+        exit(0)
--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
 var=$(cat ~/.ssh/id_rsa.pub)
-sed -i "s|ssh-rsa ...|$var|" tftp/tftpboot/ce/preseed.cfg
+sed -i "s|change_rsa|$var|" tftp/tftpboot/ce/preseed.cfg
+
 apt install -y isc-dhcp-server
 cp -rf dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
 cp -f dhcp/default /etc/default/isc-dhcp-server
+
 apt install -y tftpd-hpa pxelinux syslinux
 cp -rf tftp/default /etc/default/tftpd-hpa
 cp -rf tftp/tftpboot /srv/
@@ -17,5 +20,3 @@ cp -rf http/site.conf /etc/apache2/sites-enabled/000-default.conf
 systemctl restart isc-dhcp-server
 systemctl restart tftpd-hpa
 systemctl restart apache2
-
-apt install -y ansible
